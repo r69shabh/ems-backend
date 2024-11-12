e490e478555f0689278c455dbadced80e03e113a
r69 <143521130+r69shabh@users.noreply.github.com>
2024-11-12 14:44:00 +0530

    Add files via upload

---
+++ server/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  AUTH0_DOMAIN: 'dev-fmqe7ajp17nmk1gt.us.auth0.com',
+  AUTH0_API_AUDIENCE: 'http://localhost:8083/api/',
+  MONGO_URI: process.env.MONGO_URI || 'mongodb+srv://r69shabh:wfsGH3QoAl8PMHHL@cluster0.arsgf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+  NAMESPACE: 'http://event-management-system.com/roles'
+};
+
